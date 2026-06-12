7aaa9d15db28ef17d78718703eb814c5c5db9fa3
Kevin Alexander Mejía Mera <km830820@gmail.com>
2026-06-12 09:09:43 -0500

    Update urls.py

--- restaurante/urls.py
+++ restaurante/urls.py
@@ -55,5 +55,12 @@ urlpatterns = [
     path('manifest.json', TemplateView.as_view(template_name='usuarios/pwa/manifest.json', content_type='application/json'), name='manifest.json'),
 ]
 
-# Servir archivos media siempre (en producción lo maneja Nginx/el volumen Docker)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.urls import re_path
+from django.views.static import serve
+
+# Servir archivos media en producción (ya que static() no funciona con DEBUG=False)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
